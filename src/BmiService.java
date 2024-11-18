e58836f8c9d7613cf223888e6815bfa746ba7bee
Eugen Sabanovskiy <eugensabanovskiy123@gmail.com>
2024-11-18 17:01:44 +0300

    Third commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,8 @@
 public class BmiService {
     public double bmi(double weight, double height) {
         double index = weight / (height * height);
-        return index;
+        int indexInt;
+        indexInt = (int)index;
+        return indexInt;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double myBmi = service.bmi(98,1.87);
-        int finalBmi;
-        finalBmi = (int)myBmi;
-        System.out.println(finalBmi);
+        System.out.println(myBmi);
     }
 }
